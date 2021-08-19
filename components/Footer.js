134d5caf8685c410926a5686cc21f0f68be0a505
Jamie Biddulph <jamiebiddulph25@gmail.com>
2021-08-19 20:55:18 +0100

    Remove spotify now playing from footer

--- components/Footer.js
+++ components/Footer.js
@@ -20,7 +20,6 @@ export default function Footer() {
           <ExternalLink href="https://github.com/leerob/leerob.io">Thanks to Lee Rob for an awesome design template :)</ExternalLink>
       </div>
       <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
-      <NowPlaying />
       <div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
         <div className="flex flex-col space-y-4">
           <Link href="/">
